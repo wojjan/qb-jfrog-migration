4849b0a4c8f1fa071b854e0c6d3274b7eb267a99
Wojdat, Janusz <janusz.wojdat@intel.com>
2025-12-17 15:14:09 +0100

    add jfrog server configuration bat

---
+++ jf-config.bat
@@ -0,0 +1,5 @@
+C:\JF\jf.exe c add igk-artifactory ^
+  --artifactory-url=https://af01p-igk.devtools.intel.com/artifactory/ ^
+  --access-token=<user_token> ^
+  --user=<user> ^
+  --interactive=false
